52ad06891625c20271addbc109c406a238666dd1
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-08-08 15:35:15 +0900

    feat: chapter 4 완료 - openfeign 을 활용한 서비스 통신

--- backend/order-service/src/main/java/com/sesac/orderservice/service/OrderService.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/service/OrderService.java
@@ -1,8 +1,13 @@
 package com.sesac.orderservice.service;
 
+import com.sesac.orderservice.client.ProductServiceClient;
+import com.sesac.orderservice.client.UserServiceClient;
+import com.sesac.orderservice.client.dto.ProductDto;
+import com.sesac.orderservice.client.dto.UserDto;
 import com.sesac.orderservice.dto.OrderRequestDto;
 import com.sesac.orderservice.entity.Order;
 import com.sesac.orderservice.repository.OrderRepository;
+import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +18,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserServiceClient userServiceClient;
+    private final ProductServiceClient productServiceClient;
 
     public Order findById(Long id) {
         return orderRepository.findById(id).orElseThrow(
@@ -21,9 +28,29 @@ public class OrderService {
     }
 
     //주문 생성 (고객이 주문했을 때)
+    @Transactional
+    public Order createOrder(OrderRequestDto request) {
+        // 주문했을 때 order 만드는게 목적
+        UserDto user = userServiceClient.getUserById(request.getUserId());
+        if (user == null) throw new RuntimeException("User not found with id: " + request.getUserId());
+
+        ProductDto product =  productServiceClient.getProductById(request.getProductId());
+        if (product == null) throw new RuntimeException("Product not found with id: " + request.getProductId());
+
+        if (product.getStockQuantity() < request.getQuantity()){
+            throw new RuntimeException("Stock quantity less than requested quantity: " + request.getQuantity());
+        }
+
+        Order order = new Order();
+        order.setUserId(request.getUserId());
+        order.setTotalAmount(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+        order.setStatus("COMPLETED");
+
+        return orderRepository.save(order);
+    }
+
     //유저 정보
     //상품 정보
-    public void createOrder(OrderRequestDto request) {
-    }
+
 
 }
