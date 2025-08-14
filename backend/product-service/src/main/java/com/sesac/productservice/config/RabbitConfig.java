0e94ffdf8389fb7768024a666dbcb1d226b9bd36
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-08-14 12:28:56 +0900

    feat: 주문 생성 이벤트 처리 - 재고 차감 시 (성공) PaymentRequsetEvent 발행 로직 구현 - 재고 부족 시 (실패) PaymentFailedEvent 발행 로직 구현 - 큐, 라우팅 키 등 설정 수정 : application-dev & RabbitConfig

--- backend/product-service/src/main/java/com/sesac/productservice/config/RabbitConfig.java
+++ backend/product-service/src/main/java/com/sesac/productservice/config/RabbitConfig.java
@@ -22,6 +22,11 @@ public class RabbitConfig {
     @Value("${order.event.queue.notification}")
     private String notificationQueue;
 
+    @Value("${order.event.queue.payment-request}")
+    private String paymentRequestQueue;
+    @Value("${order.event.queue.inventory-failed}")
+    private String inventoryFailedQueue;
+
     @Value("${order.event.queue.inventory}")
     private String inventoryQueue;
 
@@ -30,6 +35,11 @@ public class RabbitConfig {
 
     @Value("${order.event.routing-key.inventory}")
     private String inventoryRoutingKey;
+
+    @Value("${order.event.routing-key.payment-request}")
+    private String paymentRoutingKey;
+    @Value("${order.event.routing-key.inventory-failed}")
+    private String InventoryRoutingKey;
 
     // Exchange 정의
     @Bean
@@ -49,6 +59,17 @@ public class RabbitConfig {
         return QueueBuilder.durable(inventoryQueue).build();
     }
 
+    @Bean
+    public Queue paymentRequestQueue() {
+        return QueueBuilder.durable(paymentRequestQueue).build();
+    }
+
+    @Bean
+    public Queue inventoryFailedQueue() {
+        return QueueBuilder.durable(inventoryFailedQueue).build();
+    }
+
+
     // Binding 정의
     @Bean
     public Binding notificationBinding() {
@@ -64,9 +85,28 @@ public class RabbitConfig {
                              .with(inventoryRoutingKey);
     }
 
+    @Bean
+    public Binding paymentRequestBinding() {
+        return BindingBuilder.bind(paymentRequestQueue())
+                .to(orderExchange())
+                .with(paymentRoutingKey);
+    }
+
+    @Bean
+    public Binding InventoryFailedBinding() {
+        return BindingBuilder.bind((inventoryFailedQueue()))
+                             .to(orderExchange())
+                             .with(InventoryRoutingKey);
+    }
+
+
     // JSON 메시지 컨버터 추가
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+
+
+
 }
---
+++ backend/product-service/src/main/java/com/sesac/productservice/event/ProductSagaHandler.java
@@ -0,0 +1,48 @@
+package com.sesac.productservice.event;
+
+import com.sesac.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductSagaHandler { // 이벤트 수신
+
+    private final ProductService productService;
+    private final ProductSagaPublisher productSagaPublisher;
+
+    @RabbitListener(queues = "${order.event.queue.inventory}") // dev.yml 에 선언함
+    public void handlerOrderEvent(OrderCreatedEvent event){ // 큐에 있으면 알아서 실행됨
+        log.info("주문 생성 이벤트 수신 - orderId : {}",event.getOrderId());
+
+        try{
+            productService.decreaseStock(event.getProductId(), event.getQuantity());
+            // 성공 시 : 결제 요청 이벤트 발행
+            PaymentRequestEvent paymentRequestEvent = new PaymentRequestEvent(
+                    event.getOrderId(),
+                    event.getUserId(),
+                    event.getProductId(),
+                    event.getQuantity(),
+                    event.getTotalAmount()
+            );
+            productSagaPublisher.publishPaymentRequest(paymentRequestEvent);
+
+
+        } catch (Exception e) {
+            log.error("재고 차감 실패 - productId : {}",event.getProductId());
+
+            // 실패 시 : 재고 차감 실패 이벤트 발행
+            InventoryFailedEvent inventoryFailedEvent = new InventoryFailedEvent(
+                    event.getOrderId(),
+                    event.getProductId(), 
+                    event.getQuantity(),
+                    "재고 부족"
+            );
+            productSagaPublisher.publishInventoryFailed(inventoryFailedEvent);
+
+        }
+    }
+}
